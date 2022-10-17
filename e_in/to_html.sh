92feca08a0fc320fc81d451884b70ab96ce20e03
uyjulian <uyjulian@gmail.com>
2022-10-17 11:47:11 -0500

    Run script in classes directory if available

--- e_in/to_html.sh
+++ e_in/to_html.sh
@@ -4,3 +4,9 @@ rm -rf ../e/
 cp -Rp ../j/ ../e/
 mv *.html ../e/contents/
 cp $(realpath $(dirname $0))/../j/contents/keywordgrep.js ../e/contents/
+export CLASSES_FILE_PROC=./classes/to_html.sh
+if [[ -f "$CLASSES_FILE_PROC" ]]; then
+pushd classes
+bash to_html.sh
+popd
+fi
--- j_in/to_html.sh
+++ j_in/to_html.sh
@@ -2,3 +2,9 @@
 perl $(realpath $(dirname $0))/to_html.pl
 cp *.html ../j/contents/
 cp $(realpath $(dirname $0))/../j/contents/keywordgrep.js ../j/contents/
+export CLASSES_FILE_PROC=./classes/to_html.sh
+if [[ -f "$CLASSES_FILE_PROC" ]]; then
+pushd classes
+bash to_html.sh
+popd
+fi
